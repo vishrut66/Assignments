d257bd95b1b5c40d65033d3949057979d83c30ac
vishrut66 <108527286+vishrut66@users.noreply.github.com>
2024-05-29 10:08:47 +0530

    Update data-Assignment.js

--- src/data-Assignment.js
+++ src/data-Assignment.js
@@ -15,31 +15,6 @@ const Assignments = [
         link: "https://drive.google.com/file/d/1a0LhS0-Ntg-q-BNvNyLHem3yUSrY3RL6/view?usp=sharing"
     },
     {
-        subject: "SE",
-        name: "Assignment 2",
-        link: "https://drive.google.com/file/d/1DOj0TwlJD0cUMgIEYVst0nacloKWHAhm/view?usp=sharing"
-    },
-    {
-        subject: "SE",
-        name: "Assignment 3",
-        link: "https://drive.google.com/file/d/10ostgwleSNVVaKEkiaRKmXbBIUtiB6W4/view?usp=sharing"
-    },
-    {
-        subject: "SE",
-        name: "Assignment 4",
-        link: "https://drive.google.com/file/d/1FsjwLP7NAmuBDt_uNe2jfJUkIr8ZDjp0/view?usp=sharing"
-    },
-    {
-        subject: "SE",
-        name: "Assignment 5",
-        link: "https://drive.google.com/file/d/1xdRJ1dhVs4DnyEqE9VexzEatAiC4ETmE/view?usp=sharing"
-    },
-    {
-        subject: "SE",
-        name: "Assignment 6",
-        link: "https://drive.google.com/file/d/1zre53_O6aSMqYoX944kuJDf-pspjXoIz/view?usp=sharing"
-    },
-    {
         subject: "DMW",
         name: "Assignment 1",
         link: "https://drive.google.com/file/d/1FArGNrvzBdgqqfJCedP1QTc_O4FA9Ooz/view?usp=sharing"
@@ -67,6 +42,11 @@ const Assignments = [
     {
         subject: "IPR",
         name: "Assignment 4",
+        link: "https://drive.google.com/file/d/1KYOgyFtCe_u6df4T0g-6ZFH3_dBKyfhY/view?usp=sharing"
+    },
+    {
+        subject: "IPR",
+        name: "Assignment 5",
         link: "https://drive.google.com/file/d/1KYOgyFtCe_u6df4T0g-6ZFH3_dBKyfhY/view?usp=sharing"
     },
     {
